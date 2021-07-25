bcc6d361a4f3525f4e7f5c302fcb5a43c81f42de
Furkan Hanci <furkanhanci265@gmail.com>
2021-07-25 20:17:14 +0300

    Gradient Background Added

---
+++ VoiceRace/VoiceRace/Extensions/UIColor+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Ext.swift
+//  VoiceRace
+//
+//  Created by Furkan Hanci on 7/25/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let bg1 = UIColor(red: 250 / 250, green: 172 / 250, blue: 168 / 250, alpha: 1)
+    static let bg2 = UIColor(red: 221 / 250, green: 214 / 250, blue: 243 / 250, alpha: 1)
+}
---
+++ VoiceRace/VoiceRace/Extensions/UIView+Ext.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Ext.swift
+//  VoiceRace
+//
+//  Created by Furkan Hanci on 7/25/21.
+//
+
+import UIKit
+
+extension UIView {
+    func makeGradient(isVertical : Bool , colorArray : [UIColor]) {
+        layer.sublayers?.filter({ $0 is CAGradientLayer }).forEach({ $0.removeFromSuperlayer() })
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = colorArray.map({ $0.cgColor })
+        
+        if isVertical {
+            //top to bottom
+            gradientLayer.locations = [0.0, 1.0]
+        } else {
+            //left to right
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        }
+        
+        backgroundColor = .clear
+        gradientLayer.frame = bounds
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
